33208bf1f91f996f6b0bb0bd99fbd851dd0cf5d7
Preetam Prakash Tewari <Preetam Prakash Tewari>
2017-02-15 11:55:13 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+const hostname = 'localhost';
+const port = 3000;
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+let contacts = require("./data");
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(cors());
+
+app.get("/api/contacts", function(req, res){
+    if(!contacts){
+        res.status(404).json({message: "No contacts found"});
+    }
+    res.json(contacts);
+});
+
+app.get("/api/contacts/:id", function(req, res){
+    const reqId = req.params.id;
+    let contact = contacts.filter(contact => {
+        return contact.id == reqId;
+    });
+    if(!contact){
+        res.status(404).json({message: "No contact found"});
+    }
+    res.json(contact[0]);
+});
+
+app.listen(port, hostname, () => {
+    console.log('Server is running');
+});
